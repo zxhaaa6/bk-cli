572062c5438aa125db4be84923c17eb0fd29abd3
zxhaaa6 <zxhaaa@hotmail.com>
2018-09-23 12:53:52 +0800

    add select options for project

--- dist/main.js
+++ dist/main.js
@@ -11,6 +11,8 @@ var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"))
 
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
+var _fs = _interopRequireDefault(require("fs"));
+
 var _path = _interopRequireDefault(require("path"));
 
 var _options = require("./options");
@@ -25,7 +27,7 @@ function _run() {
   _run = (0, _asyncToGenerator2.default)(
   /*#__PURE__*/
   _regenerator.default.mark(function _callee(rawName) {
-    var inPlace, name, toDirName;
+    var inPlace, name, toDirName, isExistDir, confirmContinue, options;
     return _regenerator.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -33,26 +35,47 @@ function _run() {
             _context.prev = 0;
             inPlace = !rawName || rawName === '.';
             name = inPlace ? _path.default.relative('../', process.cwd()) : rawName;
+            name = name.replace(/\.\.\/|\.\//g, '');
             toDirName = _path.default.resolve(rawName || '.');
-            _context.next = 6;
-            return (0, _options.confirmCreate)(inPlace, name, toDirName);
-
-          case 6:
-            _context.next = 12;
-            break;
+            isExistDir = _fs.default.existsSync(toDirName);
+            _context.next = 8;
+            return (0, _options.confirmCreate)(inPlace, isExistDir, name, toDirName);
 
           case 8:
-            _context.prev = 8;
+            confirmContinue = _context.sent;
+
+            if (confirmContinue) {
+              _context.next = 11;
+              break;
+            }
+
+            throw new Error('exist');
+
+          case 11:
+            _context.next = 13;
+            return (0, _options.selectOptions)();
+
+          case 13:
+            options = _context.sent;
+            _context.next = 20;
+            break;
+
+          case 16:
+            _context.prev = 16;
             _context.t0 = _context["catch"](0);
-            console.error(_context.t0);
+
+            if (_context.t0.message !== 'exist') {
+              console.error(_context.t0);
+            }
+
             process.exit(0);
 
-          case 12:
+          case 20:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, this, [[0, 8]]);
+    }, _callee, this, [[0, 16]]);
   }));
   return _run.apply(this, arguments);
 }
--- dist/options.js
+++ dist/options.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.confirmCreate = confirmCreate;
+exports.selectOptions = selectOptions;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
@@ -15,15 +16,15 @@ var _chalk = _interopRequireDefault(require("chalk"));
 
 var _inquirer = _interopRequireDefault(require("inquirer"));
 
-function confirmCreate(_x, _x2, _x3) {
+function confirmCreate(_x, _x2, _x3, _x4) {
   return _confirmCreate.apply(this, arguments);
 }
 
 function _confirmCreate() {
   _confirmCreate = (0, _asyncToGenerator2.default)(
   /*#__PURE__*/
-  _regenerator.default.mark(function _callee(inPlace, name, dir) {
-    var answers;
+  _regenerator.default.mark(function _callee(inPlace, isExistDir, name, dir) {
+    var answer;
     return _regenerator.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -31,13 +32,13 @@ function _confirmCreate() {
             _context.next = 2;
             return _inquirer.default.prompt([{
               type: 'confirm',
-              message: inPlace ? "Generate project in current directory?" : 'Target directory exists. Continue?',
+              message: inPlace ? "Generate project in current directory?" : isExistDir ? "Target directory ".concat(dir, " exists. Continue?") : "Generate project in directory ".concat(dir, ". Continue?"),
               name: 'ok'
             }]);
 
           case 2:
-            answers = _context.sent;
-            console.log(answers);
+            answer = _context.sent;
+            return _context.abrupt("return", answer.ok);
 
           case 4:
           case "end":
@@ -48,3 +49,69 @@ function _confirmCreate() {
   }));
   return _confirmCreate.apply(this, arguments);
 }
+
+function selectOptions() {
+  return _selectOptions.apply(this, arguments);
+}
+
+function _selectOptions() {
+  _selectOptions = (0, _asyncToGenerator2.default)(
+  /*#__PURE__*/
+  _regenerator.default.mark(function _callee2() {
+    var options;
+    return _regenerator.default.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return _inquirer.default.prompt(typeOptions);
+
+          case 2:
+            options = _context2.sent;
+            console.log(options);
+
+          case 4:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, this);
+  }));
+  return _selectOptions.apply(this, arguments);
+}
+
+var typeOptions = [{
+  type: 'list',
+  name: 'apiType',
+  message: 'Which api service do you need?',
+  choices: ['REST', 'GraphQL'],
+  filter: function filter(val) {
+    return val.toLowerCase();
+  }
+}, {
+  type: 'list',
+  name: 'jsType',
+  message: 'What type language do you like?',
+  choices: ['Javascript', 'Typescript'],
+  filter: function filter(val) {
+    return val.toLowerCase();
+  }
+}, {
+  type: 'checkbox',
+  message: 'check features needed for your project',
+  name: 'toppings',
+  choices: [{
+    name: 'Webpack',
+    checked: true
+  }, {
+    name: 'Babel'
+  }, {
+    name: 'Linter / Formatter',
+    checked: true
+  }, {
+    name: 'Unit Testing'
+  }],
+  validate: function validate(answer) {
+    return true;
+  }
+}];
