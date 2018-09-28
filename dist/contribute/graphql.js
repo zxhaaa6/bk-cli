aaa18b87766ecaadd35c6a7e55eed5e5b9c40d28
zxhaaa6 <zxhaaa@hotmail.com>
2018-09-28 14:23:40 +0800

    generator for ts-graphql-yoga

--- dist/contribute/graphql.js
+++ dist/contribute/graphql.js
@@ -21,6 +21,8 @@ var _rimraf = _interopRequireDefault(require("rimraf"));
 
 var _common = require("./common");
 
+var _ncp = _interopRequireDefault(require("../util/ncp"));
+
 function handleGraphqlContribute(_x, _x2) {
   return _handleGraphqlContribute.apply(this, arguments);
 }
@@ -96,15 +98,31 @@ function _graphqlYogaTs() {
   _graphqlYogaTs = (0, _asyncToGenerator2.default)(
   /*#__PURE__*/
   _regenerator.default.mark(function _callee2(dir, options) {
+    var SRC_DIR, MIDDLEWARE_PATH, MODULE_PATH;
     return _regenerator.default.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            if (options.features.includes('Webpack')) {
-              _fs.default.copyFileSync(_path.default.resolve(__dirname, '../templates/src/package/ts-graphql-yoga-webpack/package.json'), _path.default.resolve(dir, './package.json'));
-            }
+            SRC_DIR = _path.default.resolve(dir, './src');
+            MIDDLEWARE_PATH = _path.default.resolve(dir, './src/middleware');
+            MODULE_PATH = _path.default.resolve(dir, './src/module');
 
-          case 1:
+            _fs.default.copyFileSync(_path.default.resolve(__dirname, '../templates/src/package/ts-graphql-yoga-webpack/package.json'), _path.default.resolve(dir, './package.json'));
+
+            _fs.default.copyFileSync(_path.default.resolve(__dirname, '../templates/src/config/ts-graphql-yoga/config.ts'), _path.default.resolve(SRC_DIR, './config/config.ts'));
+
+            _context2.next = 7;
+            return (0, _ncp.default)(_path.default.resolve(__dirname, '../templates/src/app/ts-graphql/yoga/'), _path.default.resolve(SRC_DIR, './'));
+
+          case 7:
+            _context2.next = 9;
+            return (0, _ncp.default)(_path.default.resolve(__dirname, '../templates/src/middleware/ts/yoga/'), _path.default.resolve(MIDDLEWARE_PATH, './'));
+
+          case 9:
+            _context2.next = 11;
+            return (0, _ncp.default)(_path.default.resolve(__dirname, '../templates/src/module/ts-graphql/module/'), _path.default.resolve(MODULE_PATH, './'));
+
+          case 11:
           case "end":
             return _context2.stop();
         }
---
+++ dist/util/ncp.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = _default;
+
+var _ncp = _interopRequireDefault(require("ncp"));
+
+function _default(source, destination) {
+  return new Promise(function (resolve, reject) {
+    _ncp.default.limit = 16;
+    (0, _ncp.default)(source, destination, function (err) {
+      if (err) {
+        reject(new Error(err));
+      }
+
+      resolve();
+    });
+  });
+}
